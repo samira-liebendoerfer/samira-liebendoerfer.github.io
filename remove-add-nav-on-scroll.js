2222e12fe9a4dcf0b2694484c127120bdb50d6b2
Samira Liebendörfer <samira@prolog.work>
2021-06-25 12:45:12 +0200

    adds remove and add nav on scroll and link arrow styling

---
+++ remove-add-nav-on-scroll.js
@@ -0,0 +1,29 @@
+const body = document.body;
+const nav = document.querySelector("nav");
+
+const scrollUp = "scroll-up";
+const scrollDown = "scroll-down";
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+
+  if (currentScroll <= 0) {
+    body.classList.remove(scrollUp);
+    return;
+  }
+
+  if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
+    // SCROLL DOWN
+    body.classList.remove(scrollUp);
+    body.classList.add(scrollDown);
+  } else if (
+    currentScroll < lastScroll &&
+    body.classList.contains(scrollDown)
+  ) {
+    // SCROLL UP
+    body.classList.remove(scrollDown);
+    body.classList.add(scrollUp);
+  }
+  lastScroll = currentScroll;
+});
